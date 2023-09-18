d4837fad14f149f76fc7c1a87b11ad37f14df92b
Stan108 <kage74@yandex.ru>
2023-09-18 15:07:23 +0300

    aaa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/HH:mm");
         System.out.println("Текущее время: " + format.format(new Date()));
+        //
     }
 }
